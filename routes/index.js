9a094fd07268b09df2fbedcccd84a26d9f6e49c5
Ongcangco <POngcangco57@gmail.com>
2023-05-15 13:49:10 -0400

    add OAuth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.user)
+  res.render('index', { title: 'Funko HQ' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email']
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
